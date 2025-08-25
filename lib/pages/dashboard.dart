251a90e9eb6dad118b94692fb10bb69692c4bd7b
Adler Santos <adlercoelhosantos12@gmail.com>
2025-08-25 20:15:40 -0300

    feat(app): implement initial app structure with routing and placeholder components

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(body: Center(child: Text('Dashboard Page'))),
+    );
+  }
+}
