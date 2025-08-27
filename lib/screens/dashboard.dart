5a482fad8df8eef72feab08dc0c13bafd3928b23
Adler Santos <adlercoelhosantos12@gmail.com>
2025-08-26 21:10:02 -0300

    feat: implement dashboard screen with user authentication check and UI updates

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pos_fiap_fin_mobile/utils/routes.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,14 +11,46 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_auth.currentUser == null) {
+        Navigator.pushReplacementNamed(context, Routes.login);
+        return;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dashboard de transferências',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(body: Center(child: Text('Dashboard Page'))),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text(
+            'Dashboard de transferências | ${_auth.currentUser?.email}',
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () async {
+                await _auth.signOut();
+                if (!mounted) return;
+                Navigator.pushReplacementNamed(context, Routes.login);
+              },
+            ),
+          ],
+        ),
+        body: Center(child: Text('Minhas transferências')),
+      ),
     );
   }
 }
